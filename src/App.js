591e43bc8bf100b549eec42dac21a6b53ec9438b
Thomas Derflinger <tderflinger@gmail.com>
2020-08-22 19:42:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import BerlinMap from "./components/BerlinMap/BerlinMap";
+import styled from "@emotion/styled";
 
 function App() {
+  const [borough, setBorough] = useState("");
+
+  const handleBorough = (name) => {
+    setBorough(name);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <h1>Berliner Bezirke</h1>
+        <BerlinMap callback={handleBorough} />
+        <h1>{borough}</h1>
+      </Container>
+    </>
   );
 }
+
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  width: 800px;
+`;
 
 export default App;
